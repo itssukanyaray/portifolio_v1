f2a9faaa8419759aadb27a75ef9f56ce58637a37
Suman Shekhar <suman.sumanshekhar698@gmail.com>
2022-10-09 17:14:09 +0530

    fixed formatting grammar junk color-key

--- js/raven.js
+++ js/raven.js
@@ -1,35 +1,5 @@
 (function ($) {
 	"use strict";
-	//Page cursors
-
-	// document.getElementsByTagName("body")[0].addEventListener("mousemove", function(n) {
-	//     t.style.left = n.clientX + "px", 
-	// 	t.style.top = n.clientY + "px", 
-	// 	e.style.left = n.clientX + "px", 
-	// 	e.style.top = n.clientY + "px", 
-	// 	i.style.left = n.clientX + "px", 
-	// 	i.style.top = n.clientY + "px"
-	// });
-
-	// var t = document.getElementById("cursor"),
-	//     e = document.getElementById("cursor2"),
-	//     i = document.getElementById("cursor3");
-
-
-	// function n(t) {
-	//     e.classList.add("hover"), i.classList.add("hover")
-	// }
-	// function s(t) {
-	//     e.classList.remove("hover"), i.classList.remove("hover")
-	// }
-	// s();
-	// for (var r = document.querySelectorAll(".hover-target"), a = r.length - 1; a >= 0; a--) {
-	//     o(r[a])
-	// }
-	// function o(t) {
-	//     t.addEventListener("mouseover", n), t.addEventListener("mouseout", s)
-	// }
-
 	//Navigation
 	var app = function () {
 		var body = undefined;
@@ -53,7 +23,6 @@
 		init();
 	}();
 
-
 	//Switch light/dark
 
 	// $("#switch").on('click', function () {
@@ -66,5 +35,4 @@
 	// 		$("#switch").addClass("switched");
 	// 	}
 	// });
-
 })(jQuery);
--- js/scroll_to_top.js
+++ js/scroll_to_top.js
@@ -1,4 +1,4 @@
-let calcScrollValue = () =>{
+let calcScrollValue = () => {
 
     let scrollProgress = document.getElementById("progress");
     let progressValue = document.getElementById("progress-value");
@@ -6,16 +6,16 @@ let calcScrollValue = () =>{
     console.log(pos);
     let calcHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
     // console.log(calcHeight);
-    let scrollValue = Math.round((pos*100)/calcHeight);
+    let scrollValue = Math.round((pos * 100) / calcHeight);
     // console.log(scrollValue);//changing in scale of 100
-    if(pos>100){
-        scrollProgress.style.display="grid";
-    }else{
-        scrollProgress.style.display="none";
+    if (pos > 100) {
+        scrollProgress.style.display = "grid";
+    } else {
+        scrollProgress.style.display = "none";
 
     }
-    scrollProgress.addEventListener("click",()=>{
-        document.documentElement.scrollTop=0;
+    scrollProgress.addEventListener("click", () => {
+        document.documentElement.scrollTop = 0;
     });
     // scrollProgress.style.background = 'conic-gradient(#ff0000 '+scrollValue+'%, #ffffff '+scrollValue+'%)';
     scrollProgress.style.background = `conic-gradient(#1CA4B0 ${scrollValue}%, #d7d7d7 ${scrollValue}%)`;
